b77e7a2231c6cb643024c1a0ba4780714431c702
Ramy Rashad <ramy.rashad@qtech-it.com>
2021-03-02 23:53:45 +0300

    A

---
+++ CodeForce/BearAndBigBrother.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeForce
+{
+    public class BearAndBigBrother
+    {
+        public void Excute()
+        {
+            var lineOne = Console.ReadLine();
+            var lineOneValues = lineOne.Split(' ');
+            var brotherA = Convert.ToInt32(lineOneValues[0]);
+            var brotherB = Convert.ToInt32(lineOneValues[1]);
+            var result = 0;
+            while (brotherA <= brotherB)
+            {
+                brotherA = brotherA * 3;
+                brotherB = brotherB * 2;
+                result++;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
--- CodeForce/Program.cs
+++ CodeForce/Program.cs
@@ -6,7 +6,7 @@ namespace CodeForce
     {
         static void Main(string[] args)
         {
-
+           
         }
     }
 }
