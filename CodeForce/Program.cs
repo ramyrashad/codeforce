06e2ace4deccf4fc5c04efa2d80fa085b644c5ca
Ramy Rashad <ramy.rashad@qtech-it.com>
2021-03-02 23:00:34 +0300

    First Commit

---
+++ CodeForce/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeForce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
